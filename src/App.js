26ba84f1fb86ea385f6ce48807fa1feaf5982056
soppingCart <solyman100200@gmail.com>
2022-08-28 01:13:44 +0600

    search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Users } from "./data";
+import { useState } from "react";
+import Table from "./Table";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const keys=["first_name","last_name","email"]
+
+
+  console.log(Users[0])
+
+  const search = (data) => {
+    return data.filter(
+
+      (item)=>
+      keys.some(key=>item[key].toLowerCase().includes(query))
+      // (item) =>
+      //   item.first_name.toLowerCase().includes(query) ||
+      //   item.last_name.toLowerCase().includes(query) ||
+      //   item.email.toLowerCase().includes(query)
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="search...."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
+      {/* <ul className="list">
+        {Users.filter((user)=>
+        user.first_name.toLowerCase().includes(query)).map((user) => (
+          <li key={user.id} className="listItem">{user.first_name}</li>
+        ))}
+
+        <li className="listItem">Ashik</li>
+        <li className="listItem">Arif</li>
+      </ul> */}
     </div>
   );
 }
